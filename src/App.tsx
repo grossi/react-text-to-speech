afa11ea1606fdb815fc77285d5f70ca773e8646c
Gabriel Rossi <rrossigabriel@gmail.com>
2021-08-08 00:38:39 -0300

    ✨ Adds language option

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,19 @@
 import React from 'react';
 import './App.css';
+import Speak from './Speak';
 
 function App() {
   const [speakInput, setSpeakInput] = React.useState<string>("")
-  const synth = window.speechSynthesis; 
-  const speak = () => {
-    var utterThis = new SpeechSynthesisUtterance(speakInput);
-    synth.speak(utterThis);
-  }
   const handleOnChange = (event: any) => {
     setSpeakInput(event.target.value)
   }
   return (
     <div className="App">
       <header className="App-header">
-        <input type="text" value={speakInput} onKeyDown={event => {
-          if (event.key === "Enter") {
-            speak()
-          }
-        }} onChange={handleOnChange}/>
-        <button onClick={() => {speak()}}>Speak</button>
+        <input type="text" value={speakInput}onChange={handleOnChange}/>
+        <Speak lang="it">
+          {speakInput}
+        </Speak>
       </header>
     </div>
   );
